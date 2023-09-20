fcaa03ddb811f95b1f7d9e1a82e1889a3ff8b96c
Albertabrahams <halilibrahim.soyman@gmail.com>
2023-09-20 16:14:27 +0300

    edit

--- post/admin.py
+++ post/admin.py
@@ -3,6 +3,6 @@ from post.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'user', 'modified']
+    list_display = ['title', 'slug',  'user', 'modified']
     list_display_links = ['title']
     search_fields = ['title', 'content']
